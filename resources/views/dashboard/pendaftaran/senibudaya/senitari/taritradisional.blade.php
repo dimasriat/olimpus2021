e51fd80310b97ca14785862a1a94c4e8ac065d69
Dimas Riatmodjo <dimasriat@gmail.com>
2021-02-22 17:16:15 +0700

    seni tari tradisional sudah jadi dashboard dan landingnya

--- resources/views/dashboard/pendaftaran/senibudaya/senitari/taritradisional.blade.php
+++ resources/views/dashboard/pendaftaran/senibudaya/senitari/taritradisional.blade.php
@@ -1,193 +1,309 @@
 @extends("layouts.app")
 
 @section("content")
-	<h2>Pendaftaran Tari Tradisional</h2>
+<h2>Pendaftaran Tari Tradisional</h2>
 
-	<!-- DATA YANG SUDAH TERKONFIRMASI -->
-	<h3>Sudah Terkonfirmasi</h3>
-	@if (count($table_data_confirmed))
-	<table>
-		<!-- TABLE HEADING -->
-		<tr>
-			<th>id</th>
-			<th>Action</th>
-			<th>Fakultas</th>
-			<th>Email</th>
-			<th>Judul Lagu</th>
-
-			<th>Nama Peserta 1</th>
-			<th>NIM Peserta 1</th>
-			<th>WA Peserta 1</th>
-			<th>Ktm Peserta 1</th>
-			<th>Foto Peserta 1</th>
-			
-			<th>Nama Peserta 2</th>
-			<th>NIM Peserta 2</th>
-			<th>WA Peserta 2</th>
-			<th>Ktm Peserta 2</th>
-			<th>Foto Peserta 2</th>
-			
-			<th>Nama Peserta 3</th>
-			<th>NIM Peserta 3</th>
-			<th>WA Peserta 3</th>
-			<th>Ktm Peserta 3</th>
-			<th>Foto Peserta 3</th>
-			
-			<th>Nama Peserta 4</th>
-			<th>NIM Peserta 4</th>
-			<th>WA Peserta 4</th>
-			<th>Ktm Peserta 4</th>
-			<th>Foto Peserta 4</th>
-			
-			<th>Nama Peserta 5</th>
-			<th>NIM Peserta 5</th>
-			<th>WA Peserta 5</th>
-			<th>Ktm Peserta 5</th>
-			<th>Foto Peserta 5</th>
-		</tr>
-
-		<!-- TABLE BODY -->
-		@foreach($table_data_confirmed as $row)
-		<tr>
-			<td>{{ $row->id }}</td>
-			<td>
-				<form action="{{ route('pendaftaran.senitari.taritradisional.update') }}" method="POST">
-					@csrf
-					@method('put')
-					<input type="hidden" name="row_id" value="{{ $row->id }}" />
-					<input type="hidden" name="set_value" value="0" />
-					<input type="submit" value="batalkan" />
-				</form>
-			</td>
-			<td>{{ $row->fakultas }}</td>
-			<td>{{ $row->email }}</td>
-			<td>{{ $row->judul_lagu }}</td>
-
-			<td>{{ $row->nama_peserta_1 }}</td>
-			<td>{{ $row->nim_peserta_1 }}</td>
-			<td>{{ $row->no_wa_peserta_1 }}</td>
-			<td><x-image-link src="{{ asset('pendaftaran/' . $row->ktm_peserta_1) }}"/></td>
-			<td><x-image-link src="{{ asset('pendaftaran/' . $row->foto_peserta_1) }}"/></td>
-			
-			<td>{{ $row->nama_peserta_2 }}</td>
-			<td>{{ $row->nim_peserta_2 }}</td>
-			<td>{{ $row->no_wa_peserta_2 }}</td>
-			<td><x-image-link src="{{ asset('pendaftaran/' . $row->ktm_peserta_2) }}"/></td>
-			<td><x-image-link src="{{ asset('pendaftaran/' . $row->foto_peserta_2) }}"/></td>
-			
-			<td>{{ $row->nama_peserta_3 }}</td>
-			<td>{{ $row->nim_peserta_3 }}</td>
-			<td>{{ $row->no_wa_peserta_3 }}</td>
-			<td><x-image-link src="{{ asset('pendaftaran/' . $row->ktm_peserta_3) }}"/></td>
-			<td><x-image-link src="{{ asset('pendaftaran/' . $row->foto_peserta_3) }}"/></td>
-			
-			<td>{{ $row->nama_peserta_4 }}</td>
-			<td>{{ $row->nim_peserta_4 }}</td>
-			<td>{{ $row->no_wa_peserta_4 }}</td>
-			<td><x-image-link src="{{ asset('pendaftaran/' . $row->ktm_peserta_4) }}"/></td>
-			<td><x-image-link src="{{ asset('pendaftaran/' . $row->foto_peserta_4) }}"/></td>
-			
-			<td>{{ $row->nama_peserta_5 }}</td>
-			<td>{{ $row->nim_peserta_5 }}</td>
-			<td>{{ $row->no_wa_peserta_5 }}</td>
-			<td><x-image-link src="{{ asset('pendaftaran/' . $row->ktm_peserta_5) }}"/></td>
-			<td><x-image-link src="{{ asset('pendaftaran/' . $row->foto_peserta_5) }}"/></td>
-		</tr>
-		@endforeach
-	</table>
-	@endif
-
-	<!-- DATA YANG BELUM TERKONFIRMASI -->
-	<h3>Belum Terkonfirmasi</h3>
-	@if (count($table_data_not_confirmed))	
-	<table>
-		<!-- TABLE HEADING -->
-		<tr>
+<!-- DATA YANG SUDAH TERKONFIRMASI -->
+<h3>Sudah Terkonfirmasi</h3>
+@if (count($table_data_confirmed))
+<table>
+	<!-- TABLE HEADING -->
+	<tr>
 		<th>id</th>
-			<th>Action</th>
-			<th>Fakultas</th>
-			<th>Email</th>
-			<th>Judul Lagu</th>
+		<th>Action</th>
+		<th>Fakultas</th>
+		<th>Judul Karya</th>
 
-			<th>Nama Peserta 1</th>
-			<th>NIM Peserta 1</th>
-			<th>WA Peserta 1</th>
-			<th>Ktm Peserta 1</th>
-			<th>Foto Peserta 1</th>
-			
-			<th>Nama Peserta 2</th>
-			<th>NIM Peserta 2</th>
-			<th>WA Peserta 2</th>
-			<th>Ktm Peserta 2</th>
-			<th>Foto Peserta 2</th>
-			
-			<th>Nama Peserta 3</th>
-			<th>NIM Peserta 3</th>
-			<th>WA Peserta 3</th>
-			<th>Ktm Peserta 3</th>
-			<th>Foto Peserta 3</th>
-			
-			<th>Nama Peserta 4</th>
-			<th>NIM Peserta 4</th>
-			<th>WA Peserta 4</th>
-			<th>Ktm Peserta 4</th>
-			<th>Foto Peserta 4</th>
-			
-			<th>Nama Peserta 5</th>
-			<th>NIM Peserta 5</th>
-			<th>WA Peserta 5</th>
-			<th>Ktm Peserta 5</th>
-			<th>Foto Peserta 5</th>
-		</tr>
+		<th>Nama Ketua</th>
+		<th>NIM Ketua</th>
+		<th>TTL Ketua</th>
+		<th>Alamat Ketua</th>
+		<th>No Wa Ketua</th>
+		<th>Email Ketua</th>
+		<th>Jurusan / Angkatan Ketua</th>
+		<th>KTM Ketua</th>
+		<th>Foto Ketua</th>
+		
+		<th>Nama Anggota 1</th>
+		<th>NIM Anggota 1</th>
+		<th>TTL Anggota 1</th>
+		<th>Alamat Anggota 1</th>
+		<th>No Wa Anggota 1</th>
+		<th>Email Anggota 1</th>
+		<th>Jurusan / Angkatan Anggota 1</th>
+		<th>KTM Anggota 1</th>
+		<th>Foto Anggota 1</th>
 
-		<!-- TABLE BODY -->
-		@foreach($table_data_not_confirmed as $row)
-		<tr>
-			<td>{{ $row->id }}</td>
-			<td>
-				<form action="{{ route('pendaftaran.senitari.taritradisional.update') }}" method="POST">
-					@csrf
-					@method('put')
-					<input type="hidden" name="row_id" value="{{ $row->id }}" />
-					<input type="hidden" name="set_value" value="1" />
-					<input type="submit" value="konfirmasi" />
-				</form>
-			</td>
-			<td>{{ $row->fakultas }}</td>
-			<td>{{ $row->email }}</td>
-			<td>{{ $row->judul_lagu }}</td>
+		<th>Nama Anggota 2</th>
+		<th>NIM Anggota 2</th>
+		<th>TTL Anggota 2</th>
+		<th>Alamat Anggota 2</th>
+		<th>No Wa Anggota 2</th>
+		<th>Email Anggota 2</th>
+		<th>Jurusan / Angkatan Anggota 2</th>
+		<th>KTM Anggota 2</th>
+		<th>Foto Anggota 2</th>
 
-			<td>{{ $row->nama_peserta_1 }}</td>
-			<td>{{ $row->nim_peserta_1 }}</td>
-			<td>{{ $row->no_wa_peserta_1 }}</td>
-			<td><x-image-link src="{{ asset('pendaftaran/' . $row->ktm_peserta_1) }}"/></td>
-			<td><x-image-link src="{{ asset('pendaftaran/' . $row->foto_peserta_1) }}"/></td>
-			
-			<td>{{ $row->nama_peserta_2 }}</td>
-			<td>{{ $row->nim_peserta_2 }}</td>
-			<td>{{ $row->no_wa_peserta_2 }}</td>
-			<td><x-image-link src="{{ asset('pendaftaran/' . $row->ktm_peserta_2) }}"/></td>
-			<td><x-image-link src="{{ asset('pendaftaran/' . $row->foto_peserta_2) }}"/></td>
-			
-			<td>{{ $row->nama_peserta_3 }}</td>
-			<td>{{ $row->nim_peserta_3 }}</td>
-			<td>{{ $row->no_wa_peserta_3 }}</td>
-			<td><x-image-link src="{{ asset('pendaftaran/' . $row->ktm_peserta_3) }}"/></td>
-			<td><x-image-link src="{{ asset('pendaftaran/' . $row->foto_peserta_3) }}"/></td>
-			
-			<td>{{ $row->nama_peserta_4 }}</td>
-			<td>{{ $row->nim_peserta_4 }}</td>
-			<td>{{ $row->no_wa_peserta_4 }}</td>
-			<td><x-image-link src="{{ asset('pendaftaran/' . $row->ktm_peserta_4) }}"/></td>
-			<td><x-image-link src="{{ asset('pendaftaran/' . $row->foto_peserta_4) }}"/></td>
-			
-			<td>{{ $row->nama_peserta_5 }}</td>
-			<td>{{ $row->nim_peserta_5 }}</td>
-			<td>{{ $row->no_wa_peserta_5 }}</td>
-			<td><x-image-link src="{{ asset('pendaftaran/' . $row->ktm_peserta_5) }}"/></td>
-			<td><x-image-link src="{{ asset('pendaftaran/' . $row->foto_peserta_5) }}"/></td>
-		</tr>
-		@endforeach
-	</table>
-	@endif
+		<th>Nama Anggota 3</th>
+		<th>NIM Anggota 3</th>
+		<th>TTL Anggota 3</th>
+		<th>Alamat Anggota 3</th>
+		<th>No Wa Anggota 3</th>
+		<th>Email Anggota 3</th>
+		<th>Jurusan / Angkatan Anggota 3</th>
+		<th>KTM Anggota 3</th>
+		<th>Foto Anggota 3</th>
+
+		<th>Nama Anggota 4</th>
+		<th>NIM Anggota 4</th>
+		<th>TTL Anggota 4</th>
+		<th>Alamat Anggota 4</th>
+		<th>No Wa Anggota 4</th>
+		<th>Email Anggota 4</th>
+		<th>Jurusan / Angkatan Anggota 4</th>
+		<th>KTM Anggota 4</th>
+		<th>Foto Anggota 4</th>
+	</tr>
+
+	<!-- TABLE BODY -->
+	@foreach($table_data_confirmed as $row)
+	<tr>
+		<td>{{ $row->id }}</td>
+		<td>
+			<form action="{{ route('pendaftaran.senitari.taritradisional.update') }}" method="POST">
+				@csrf
+				@method('put')
+				<input type="hidden" name="row_id" value="{{ $row->id }}" />
+				<input type="hidden" name="set_value" value="0" />
+				<input type="submit" value="batalkan" />
+			</form>
+		</td>
+		<td>{{ $row->fakultas }}</td>
+		<td>{{ $row->judul_karya }}</td>
+
+		<td>{{ $row->nama_ketua }}</td>
+		<td>{{ $row->nim_ketua }}</td>
+		<td>{{ $row->ttl_ketua }}</td>
+		<td>{{ $row->alamat_ketua }}</td>
+		<td>{{ $row->no_wa_ketua }}</td>
+		<td>{{ $row->email_ketua }}</td>
+		<td>{{ $row->jurusan_angkatan_ketua }}</td>
+		<td>
+			<x-image-link src="{{ asset('pendaftaran/' . $row->ktm_ketua) }}" />
+		</td>
+		<td>
+			<x-image-link src="{{ asset('pendaftaran/' . $row->foto_ketua) }}" />
+		</td>
+
+		<td>{{ $row->nama_anggota_1 }}</td>
+		<td>{{ $row->nim_anggota_1 }}</td>
+		<td>{{ $row->ttl_anggota_1 }}</td>
+		<td>{{ $row->alamat_anggota_1 }}</td>
+		<td>{{ $row->no_wa_anggota_1 }}</td>
+		<td>{{ $row->email_anggota_1 }}</td>
+		<td>{{ $row->jurusan_angkatan_anggota_1 }}</td>
+		<td>
+			<x-image-link src="{{ asset('pendaftaran/' . $row->ktm_anggota_1) }}" />
+		</td>
+		<td>
+			<x-image-link src="{{ asset('pendaftaran/' . $row->foto_anggota_1) }}" />
+		</td>
+
+		<td>{{ $row->nama_anggota_2 }}</td>
+		<td>{{ $row->nim_anggota_2 }}</td>
+		<td>{{ $row->ttl_anggota_2 }}</td>
+		<td>{{ $row->alamat_anggota_2 }}</td>
+		<td>{{ $row->no_wa_anggota_2 }}</td>
+		<td>{{ $row->email_anggota_2 }}</td>
+		<td>{{ $row->jurusan_angkatan_anggota_2 }}</td>
+		<td>
+			<x-image-link src="{{ asset('pendaftaran/' . $row->ktm_anggota_2) }}" />
+		</td>
+		<td>
+			<x-image-link src="{{ asset('pendaftaran/' . $row->foto_anggota_2) }}" />
+		</td>
+
+		<td>{{ $row->nama_anggota_3 }}</td>
+		<td>{{ $row->nim_anggota_3 }}</td>
+		<td>{{ $row->ttl_anggota_3 }}</td>
+		<td>{{ $row->alamat_anggota_3 }}</td>
+		<td>{{ $row->no_wa_anggota_3 }}</td>
+		<td>{{ $row->email_anggota_3 }}</td>
+		<td>{{ $row->jurusan_angkatan_anggota_3 }}</td>
+		<td>
+			<x-image-link src="{{ asset('pendaftaran/' . $row->ktm_anggota_3) }}" />
+		</td>
+		<td>
+			<x-image-link src="{{ asset('pendaftaran/' . $row->foto_anggota_3) }}" />
+		</td>
+
+		<td>{{ $row->nama_anggota_4 }}</td>
+		<td>{{ $row->nim_anggota_4 }}</td>
+		<td>{{ $row->ttl_anggota_4 }}</td>
+		<td>{{ $row->alamat_anggota_4 }}</td>
+		<td>{{ $row->no_wa_anggota_4 }}</td>
+		<td>{{ $row->email_anggota_4 }}</td>
+		<td>{{ $row->jurusan_angkatan_anggota_4 }}</td>
+		<td>
+			<x-image-link src="{{ asset('pendaftaran/' . $row->ktm_anggota_4) }}" />
+		</td>
+		<td>
+			<x-image-link src="{{ asset('pendaftaran/' . $row->foto_anggota_4) }}" />
+		</td>
+	</tr>
+	@endforeach
+</table>
+@endif
+
+<!-- DATA YANG BELUM TERKONFIRMASI -->
+<h3>Belum Terkonfirmasi</h3>
+@if (count($table_data_not_confirmed))
+<table>
+	<!-- TABLE HEADING -->
+	<tr>
+		<th>id</th>
+		<th>Action</th>
+		<th>Fakultas</th>
+		<th>Judul Karya</th>
+
+		<th>Nama Ketua</th>
+		<th>NIM Ketua</th>
+		<th>TTL Ketua</th>
+		<th>Alamat Ketua</th>
+		<th>No Wa Ketua</th>
+		<th>Email Ketua</th>
+		<th>Jurusan / Angkatan Ketua</th>
+		<th>KTM Ketua</th>
+		<th>Foto Ketua</th>
+
+		<th>Nama Anggota 1</th>
+		<th>NIM Anggota 1</th>
+		<th>TTL Anggota 1</th>
+		<th>Alamat Anggota 1</th>
+		<th>No Wa Anggota 1</th>
+		<th>Email Anggota 1</th>
+		<th>Jurusan / Angkatan Anggota 1</th>
+		<th>KTM Anggota 1</th>
+		<th>Foto Anggota 1</th>
+
+		<th>Nama Anggota 2</th>
+		<th>NIM Anggota 2</th>
+		<th>TTL Anggota 2</th>
+		<th>Alamat Anggota 2</th>
+		<th>No Wa Anggota 2</th>
+		<th>Email Anggota 2</th>
+		<th>Jurusan / Angkatan Anggota 2</th>
+		<th>KTM Anggota 2</th>
+		<th>Foto Anggota 2</th>
+
+		<th>Nama Anggota 3</th>
+		<th>NIM Anggota 3</th>
+		<th>TTL Anggota 3</th>
+		<th>Alamat Anggota 3</th>
+		<th>No Wa Anggota 3</th>
+		<th>Email Anggota 3</th>
+		<th>Jurusan / Angkatan Anggota 3</th>
+		<th>KTM Anggota 3</th>
+		<th>Foto Anggota 3</th>
+
+		<th>Nama Anggota 4</th>
+		<th>NIM Anggota 4</th>
+		<th>TTL Anggota 4</th>
+		<th>Alamat Anggota 4</th>
+		<th>No Wa Anggota 4</th>
+		<th>Email Anggota 4</th>
+		<th>Jurusan / Angkatan Anggota 4</th>
+		<th>KTM Anggota 4</th>
+		<th>Foto Anggota 4</th>
+	</tr>
+
+	<!-- TABLE BODY -->
+	@foreach($table_data_not_confirmed as $row)
+	<tr>
+		<td>{{ $row->id }}</td>
+		<td>
+			<form action="{{ route('pendaftaran.senitari.taritradisional.update') }}" method="POST">
+				@csrf
+				@method('put')
+				<input type="hidden" name="row_id" value="{{ $row->id }}" />
+				<input type="hidden" name="set_value" value="1" />
+				<input type="submit" value="konfirmasi" />
+			</form>
+		</td>
+		<td>{{ $row->fakultas }}</td>
+		<td>{{ $row->judul_karya }}</td>
+
+		<td>{{ $row->nama_ketua }}</td>
+		<td>{{ $row->nim_ketua }}</td>
+		<td>{{ $row->ttl_ketua }}</td>
+		<td>{{ $row->alamat_ketua }}</td>
+		<td>{{ $row->no_wa_ketua }}</td>
+		<td>{{ $row->email_ketua }}</td>
+		<td>{{ $row->jurusan_angkatan_ketua }}</td>
+		<td>
+			<x-image-link src="{{ asset('pendaftaran/' . $row->ktm_ketua) }}" />
+		</td>
+		<td>
+			<x-image-link src="{{ asset('pendaftaran/' . $row->foto_ketua) }}" />
+		</td>
+
+		<td>{{ $row->nama_anggota_1 }}</td>
+		<td>{{ $row->nim_anggota_1 }}</td>
+		<td>{{ $row->ttl_anggota_1 }}</td>
+		<td>{{ $row->alamat_anggota_1 }}</td>
+		<td>{{ $row->no_wa_anggota_1 }}</td>
+		<td>{{ $row->email_anggota_1 }}</td>
+		<td>{{ $row->jurusan_angkatan_anggota_1 }}</td>
+		<td>
+			<x-image-link src="{{ asset('pendaftaran/' . $row->ktm_anggota_1) }}" />
+		</td>
+		<td>
+			<x-image-link src="{{ asset('pendaftaran/' . $row->foto_anggota_1) }}" />
+		</td>
+
+		<td>{{ $row->nama_anggota_2 }}</td>
+		<td>{{ $row->nim_anggota_2 }}</td>
+		<td>{{ $row->ttl_anggota_2 }}</td>
+		<td>{{ $row->alamat_anggota_2 }}</td>
+		<td>{{ $row->no_wa_anggota_2 }}</td>
+		<td>{{ $row->email_anggota_2 }}</td>
+		<td>{{ $row->jurusan_angkatan_anggota_2 }}</td>
+		<td>
+			<x-image-link src="{{ asset('pendaftaran/' . $row->ktm_anggota_2) }}" />
+		</td>
+		<td>
+			<x-image-link src="{{ asset('pendaftaran/' . $row->foto_anggota_2) }}" />
+		</td>
+
+		<td>{{ $row->nama_anggota_3 }}</td>
+		<td>{{ $row->nim_anggota_3 }}</td>
+		<td>{{ $row->ttl_anggota_3 }}</td>
+		<td>{{ $row->alamat_anggota_3 }}</td>
+		<td>{{ $row->no_wa_anggota_3 }}</td>
+		<td>{{ $row->email_anggota_3 }}</td>
+		<td>{{ $row->jurusan_angkatan_anggota_3 }}</td>
+		<td>
+			<x-image-link src="{{ asset('pendaftaran/' . $row->ktm_anggota_3) }}" />
+		</td>
+		<td>
+			<x-image-link src="{{ asset('pendaftaran/' . $row->foto_anggota_3) }}" />
+		</td>
+
+		<td>{{ $row->nama_anggota_4 }}</td>
+		<td>{{ $row->nim_anggota_4 }}</td>
+		<td>{{ $row->ttl_anggota_4 }}</td>
+		<td>{{ $row->alamat_anggota_4 }}</td>
+		<td>{{ $row->no_wa_anggota_4 }}</td>
+		<td>{{ $row->email_anggota_4 }}</td>
+		<td>{{ $row->jurusan_angkatan_anggota_4 }}</td>
+		<td>
+			<x-image-link src="{{ asset('pendaftaran/' . $row->ktm_anggota_4) }}" />
+		</td>
+		<td>
+			<x-image-link src="{{ asset('pendaftaran/' . $row->foto_anggota_4) }}" />
+		</td>
+	</tr>
+	@endforeach
+</table>
+@endif
 @endsection
