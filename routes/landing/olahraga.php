1c6ba744c770862b347bd19c7a29913f524c0518
Dimas Riatmodjo <dimasriat@gmail.com>
2021-02-23 18:24:05 +0700

    halaman esports diperdetail lagi

--- routes/landing/olahraga.php
+++ routes/landing/olahraga.php
@@ -6,5 +6,17 @@ use App\Http\Controllers\LandingMaintenanceController;
 Route::get('/olahraga/catur', [LandingMaintenanceController::class, "index"])
 	->name('landing.olahraga.catur.index');
 
-Route::get('/olahraga/esports', [LandingMaintenanceController::class, "index"])
-	->name('landing.olahraga.esports.index');
+Route::get('/olahraga/esports', function () {
+	$api = json_decode(file_get_contents(__DIR__ . "/../../resources/api/api.json"), true);
+	return view('landing.olahraga.esports.index', ['api' => $api]);
+})->name('landing.olahraga.esports.index');
+
+
+Route::get('/olahraga/esports/mobilelegends', [LandingMaintenanceController::class, "index"])
+	->name('landing.olahraga.esports.mobilelegends.index');
+Route::get('/olahraga/esports/pubgmobile', [LandingMaintenanceController::class, "index"])
+	->name('landing.olahraga.esports.pubgmobile.index');
+Route::get('/olahraga/esports/dota2', [LandingMaintenanceController::class, "index"])
+	->name('landing.olahraga.esports.dota2.index');
+Route::get('/olahraga/esports/pointblank', [LandingMaintenanceController::class, "index"])
+	->name('landing.olahraga.esports.pointblank.index');
