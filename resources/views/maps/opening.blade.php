c4dbbd0855560fbeb6a75bdb3b56602225dca73d
Dimas Riatmodjo <dimasriat@gmail.com>
2021-03-20 06:02:43 +0700

    halaman opening udah jadi

--- resources/views/maps/opening.blade.php
+++ resources/views/maps/opening.blade.php
@@ -19,7 +19,7 @@
 		<main>
 			<div id="content">
 				<div id="video">
-					<iframe  src="https://www.youtube.com/embed/p8PCl7HAhuE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+					<iframe src="https://www.youtube.com/embed/p8PCl7HAhuE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 				</div>
 				<div id="rundown">
 					<h2>Rundown</h2>
@@ -83,17 +83,31 @@
 					</table>
 				</div>
 			</div>
-			<!-- <div id="chats">
-				<div id="read"></div>
-				<div id="create"></div>
-			</div> -->
+			<div id="chats">
+				<div id="write">
+					<h2>Live Chat</h2>
+					<form onsubmit="return sendMessage();">
+						<input type="text" name="username" id="username" placeholder="username" />
+						<input type="hidden" name="color" id="color" value="" />
+						<textarea name="message" id="message" placeholder="message" autocomplete="FALSE"></textarea>
+						<input type="submit" value="send" />
+					</form>
+					<p style="font-family: monospace">
+						Untuk menambahkan emoji pada Windows 10, tekan Windows + tanda titik (.) di keyboard
+					</p>
+				</div>
+				<div id="read">
+					<div id="chatbox"></div>
+				</div>
+			</div>
 		</main>
 		<footer>
 			<div>Copyright © 2021 Olimpus UNS. All rights reserved.</div>
 		</footer>
 	</div>
-	<script src="{{ asset('js/maps.js') }}"></script>
-	<script src="{{ asset('js/jquery.min.js') }}"></script>
+	<script src="https://www.gstatic.com/firebasejs/8.3.1/firebase-app.js"></script>
+	<script src="https://www.gstatic.com/firebasejs/8.3.1/firebase-database.js"></script>
+	<script src="{{ asset('js/opening.js') }}"></script>
 </body>
 
 </html>
