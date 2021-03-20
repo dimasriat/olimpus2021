35367d2c2f34328ad07319e38b8881acc9dda42c
Dimas Riatmodjo <dimasriat@gmail.com>
2021-03-20 18:36:59 +0700

    fix opening music auto off

--- public/js/maps2.js
+++ public/js/maps2.js
@@ -1,5 +1,12 @@
 const player = document.getElementById("player");
 document.getElementById("explore").onclick = function () {
     document.getElementById("overlay").style.display = "none";
-    player.play()
+    player.play();
+};
+document.getElementById("opening-btn").onclick = function () {
+    player.pause();
+};
+
+document.getElementById("opening-mobile-btn").onclick = function () {
+    player.pause();
 };
